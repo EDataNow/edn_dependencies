da065c006ebe1558c5343433939710421f4ec54a
Stacey Vetzal <stacey@vetzal.com>
2014-11-05 10:58:42 -0500

    splitting make

--- util.sh
+++ util.sh
@@ -1,5 +1,3 @@
-#!/bin/sh 
-
 # External tooling
 export MAKE_FMWK=external/make-fmwk/make-fmwk.sh
 export CURL_BUILD=external/curl-ios-build-scripts/build_curl
@@ -94,72 +92,4 @@ build_binary()
     make
 }
 
-# Specifics for building out gmock
-build_gmock()
-{
-  # GMock
-
-  PRODUCT=gmock-1.7.0
-  copts=""
-
-  rm -rf $PRODUCT
-  unzip ${PRODUCT}.zip
-
-  cd $PRODUCT
-
-  buildit x86_64 iPhoneSimulator $copts
-
-  cd ..
-
-  mkdir -p gmock.framework/Headers/gmock/internal
-  cp $PRODUCT/include/gmock/gmock.h gmock.framework/Headers/gmock.h
-  cp $PRODUCT/include/gmock/*.h gmock.framework/Headers/gmock/
-  cp $PRODUCT/include/gmock/*.pump gmock.framework/Headers/gmock/
-  cp $PRODUCT/include/gmock/internal/*.h gmock.framework/Headers/gmock/internal/
-  cp $PRODUCT/include/gmock/internal/*.pump gmock.framework/Headers/gmock/internal/
-  cp $PRODUCT/lib/.libs/libgmock.a gmock.framework/libgmock.a
-  cp $PRODUCT/lib/.libs/libgmock_main.a gmock.framework/libgmock_main.a
-  cp gmock-staticFramework-Info.plist gmock.framework/
-}
-
-# Specifics for building out jansson
-build_jansson()
-{
-  # BUILD JANSSON
-
-  PRODUCT=jansson-2.7
-  copts=""
-
-  rm -rf $PRODUCT
-  tar -xzf ${PRODUCT}.tar.gz
-
-  cd $PRODUCT
-
-  buildit arm64 iPhoneOS $copts
-  $AR rv libjansson.arm64.a src/.libs/*.o
-
-  buildit armv7s iPhoneOS $copts
-  $AR rv libjansson.armv7s.a src/.libs/*.o
-
-  buildit armv7 iPhoneOS $copts
-  $AR rv libjansson.armv7.a src/.libs/*.o
-
-  buildit i386 iPhoneSimulator $copts
-  $AR rv libjansson.i386.a src/.libs/*.o
-
-  lipo -create libjansson.arm64.a libjansson.armv7s.a libjansson.armv7.a libjansson.i386.a -output libjansson.a
-
-  cd ..
-
-  # BUILD jansson.framework
-
-  mkdir -p jansson.framework/Headers
-  cp $PRODUCT/src/jansson.h jansson.framework/Headers/
-  cp $PRODUCT/src/jansson_config.h jansson.framework/Headers/
-  cp $PRODUCT/libjansson.a jansson.framework/jansson
-  cp jansson-staticFramework-Info.plist jansson.framework/
-
-  rm -rf $PRODUCT
-}
-
-build_jansson
+echo "It isn't appropriate to run this script. Use a build script for the framework you want to build."
