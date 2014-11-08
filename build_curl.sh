31a48f1d70faac2812647a09d964d90f57c5051a
Stacey Vetzal <stacey@vetzal.com>
2014-11-08 09:22:00 -0500

    updates to curl

--- build_curl.sh
+++ build_curl.sh
@@ -2,4 +2,20 @@
 
 source build-util.sh
 
-$RUN_DIR/external/curl-ios-build-scripts/build_curl --archs armv7,armv7s,arm64,x86_64 --result-dir $ARCH_DIR/curl
+assemble_curl_static_framework() {
+  ensure_dir_exists $FRAMEWORKS_DIR/curl.framework/Headers
+
+  cp $ARCHS_DIR/ios-appstore/lib/libcurl.a $FRAMEWORKS_DIR/curl.framework/libcurl
+  cp $ARCHS_DIR/ios-appstore/include/* $FRAMEWORKS_DIR/curl.framework/Headers
+
+  cp $PLIST_DIR/curl-staticFramework-Info.plist $FRAMEWORKS_DIR/curl.framework/
+}
+
+# Bug in current curl-ios-build-scripts
+# ensure_dir_exists $ARCHS_DIR/curl
+ensure_dir_exists $ARCHS_DIR
+
+$RUN_DIR/external/curl-ios-build-scripts/build_curl --archs armv7,armv7s,arm64,x86_64 --no-cleanup --work-dir $BUILD_DIR/curl --result-dir $ARCHS_DIR/curl
+
+# Bug in current curl-ios-build-scripts
+mv $RUN_DIR/curl $ARCHS_DIR
