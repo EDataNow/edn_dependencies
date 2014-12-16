3348d433fff5164de36641739dee03d116e9b1b3
Stacey Vetzal <stacey@vetzal.com>
2014-12-16 13:51:49 -0500

    fixing up curl build to include i386 again

--- build_curl.sh
+++ build_curl.sh
@@ -5,17 +5,17 @@ source build-util.sh
 assemble_curl_static_framework() {
   ensure_dir_exists $FRAMEWORKS_DIR/curl.framework/Headers
 
-  cp $ARCHS_DIR/curl/ios-appstore/lib/libcurl.a $FRAMEWORKS_DIR/curl.framework/curl
-  cp $ARCHS_DIR/curl/ios-appstore/include/* $FRAMEWORKS_DIR/curl.framework/Headers
+  cp $ARCHS_DIR/curl/ios-dev/lib/libcurl.a $FRAMEWORKS_DIR/curl.framework/curl
+  cp $ARCHS_DIR/curl/ios-dev/include/* $FRAMEWORKS_DIR/curl.framework/Headers
 
   cp $PLIST_DIR/curl-staticFramework-Info.plist $FRAMEWORKS_DIR/curl.framework/
 }
 
 # Bug in current curl-ios-build-scripts
-# ensure_dir_exists $ARCHS_DIR/curl
-ensure_dir_exists $ARCHS_DIR
+ensure_dir_exists $ARCHS_DIR/curl
+#ensure_dir_exists $ARCHS_DIR
 
-$RUN_DIR/external/curl-ios-build-scripts/build_curl --archs armv7,armv7s,arm64,x86_64 --no-cleanup --work-dir $BUILD_DIR/curl --result-dir $ARCHS_DIR/curl
+$RUN_DIR/external/curl-ios-build-scripts/build_curl --archs i386,armv7,armv7s,arm64,x86_64 --no-cleanup --work-dir $BUILD_DIR/curl --result-dir $ARCHS_DIR/curl
 
 # Bug in current curl-ios-build-scripts, ignores --work-dir and --result-dir directives
 rm -rf $ARCHS_DIR/curl
